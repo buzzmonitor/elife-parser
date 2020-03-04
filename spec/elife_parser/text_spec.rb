db3ba16603ebfe256be0970b49acc1de48854f84
Thomaz Diniz <thomaz_diniz@hotmail.com>
2020-03-03 21:16:49 -0300

    Update text_spec.rb

--- spec/elife_parser/text_spec.rb
+++ spec/elife_parser/text_spec.rb
@@ -17,9 +17,14 @@ RSpec.describe ElifeParser::Text do
 
   describe "modified_text" do
     it "should remove special caracters and change emojis" do
-      expect(described_class.new("@livia.vilaca ##opa/&eba 🔥 🐷🐽.$ :?  .$ - -a º ρε👽😀☂ ❤ 华み원 ❤ ❤️ ❤ ❤️")
-        .modified_text).to eql(" @livia.vilaca ##opa/&eba  :fire:   :pig:  :pig_nose:   :?   - -a º ρε :alien:  :grinning: ☂  :heart:  华み원  :heart:   :heart: :heart:   :heart: ")
+      expect(described_class.new("@livia.vilaca ##opa/&eba 🔥 🐷🐽.$ :?  .$ - -a º ρε👽😀☂❤华み원❤")
+        .modified_text).to eql(" @livia.vilaca ##opa/&eba :fire: :pig: :pig_nose: a ρε :alien: :grinning: :heart: :heart: ")
+    end
+    it "should parse unicode heart and convert hearts" do
+      expect(described_class.new("❤️ e 🔥 e ❤")
+        .modified_text).to eql(" :heart: e :fire: e :heart: ")
     end
   end
+
 
 end
