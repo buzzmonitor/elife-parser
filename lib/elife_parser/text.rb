790d3836af6e461929a4c93bfc0342953677037b
LiviaVilaca <livia.catarine@gmail.com>
2020-03-04 12:12:23 -0300

    fix term heart

--- lib/elife_parser/text.rb
+++ lib/elife_parser/text.rb
@@ -15,11 +15,6 @@ module ElifeParser
       end
     end
 
-    def parse_unicode_emojis text
-      # parse old black_hevy_heart to the new one 
-      text.gsub("❤", "❤️")
-    end
-
     def original_text
       @original_text ||= begin
         # remove white spaces
@@ -46,7 +41,7 @@ module ElifeParser
         final_text = final_text.gsub(/[^\.\+\w\s\@\#\&\❤\u0370-\u03ff\u1f00-\u1fff\/\u{1f300}-\u{1f5ff}\u{1f600}-\u{1f64f}]/," ")
         
         #faz o parsing de um coração para outro, se não quiser fazer a conversão dos corações é só comentar
-        final_text = parse_unicode_emojis final_text
+        final_text = ElifeParser::EMOJI_GSUB_PROC.call(final_text)
         
         final_text = EmojiParser.parse_unicode(final_text) { |emoji| " :#{emoji.name}: " }.gsub(skin_tone_re, "")
         
